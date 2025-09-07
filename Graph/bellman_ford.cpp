e25fdc48fa43b1c10465fb93708c18cb97a23ef6
miftah-mj <jannat27120@gmail.com>
2025-09-07 21:11:34 +0600

    add graph algorithms

--- Graph/bellman_ford.cpp
+++ Graph/bellman_ford.cpp
@@ -26,6 +26,28 @@ void bellmanFord() {
             }
         }
     }
+
+    // check for negative weight cycles
+    bool negativeCycle = false;
+    for(auto edge : edges) {
+        int u = edge.u;
+        int v = edge.v;
+        int w = edge.w;
+        if(dist[u] != INT_MAX && dist[u] + w < dist[v]) {
+            negativeCycle = true;
+            break;
+        }
+    }
+    if(negativeCycle){
+        cout << "Graph contains negative weight cycle" << endl;
+    } 
+    else{
+        cout << "No negative weight cycle detected" << endl;
+        cout << "Shortest distances from source node 0:" << endl;
+        for(int i = 0; i < n; i++){
+            cout << i << " -> " << dist[i] << endl;
+        }
+    }
 }
 
 int main() {
@@ -35,6 +57,7 @@ int main() {
         int u, v, w;
         cin >> u >> v >> w;
         edges.push_back(Edge(u, v, w));
+        // edges.push_back(Edge(v, u, w)); // for undirected graph
     }
     for(auto edge : edges){
         cout << edge.u << " " << edge.v << " " << edge.w << endl;
@@ -45,11 +68,6 @@ int main() {
     dist[0] = 0; // source node
 
     bellmanFord();
-
-    cout << "Shortest distances from source node 0:" << endl;
-    for(int i = 0; i < n; i++){
-        cout << i << " -> " << dist[i] << endl;
-    }
 
    return 0;
 }
